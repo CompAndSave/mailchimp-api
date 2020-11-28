8e8647e3af0cb85bb302b6d1ed278168cbf56634
Andrew Y <andrew@compandsave.com>
2020-11-28 15:27:12 -0800

    import mc campaign data

--- classes/mailchimp/MailChimp.js
+++ classes/mailchimp/MailChimp.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const MongoDB = require("../mongoDB/MongoDB");
 
 class MailChimp {
@@ -5,10 +6,33 @@ class MailChimp {
     this.listData = new MongoDB("listData");
     this.campaignData = new MongoDB("campaignData");
     this.campaignReport = new MongoDB("campaignReport");
+    this.audienceId = {
+      cas: process.env.CAS_AUDIENCE_LIST_ID,
+      ci: process.env.CI_AUDIENCE_LIST_ID,
+      ti: process.env.TI_AUDIENCE_LIST_ID
+    };
+    this.auth = {
+      auth: {
+        username: process.env.MC_USERNAME,
+        password: process.env.MC_API_KEY
+      }
+    };
+  }
+
+  async getCampaignData(site, count, sinceSendTime, sortField) {
+    return Promise.resolve((await axios.get(`${process.env.MC_API_URL}campaigns?&count=${count}&since_send_time=${sinceSendTime}&list_id=${this.audienceId[site]}&sort_field=${sortField}`, this.auth)).data);
+  }
+
+  async getCampaignContentData(campaignId) {
+    return Promise.resolve((await axios.get(`${process.env.MC_API_URL}campaigns/${campaignId}/content`, this.auth)));
   }
 
   async getListData() {
     return await this.listData.getAllData();
+  }
+
+  async insertCampaignData(insertArr) {
+    return await this.campaignData.insertBulkUnOrdered(insertArr);
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,74 @@
 var express = require('express');
 var router = express.Router();
 const asyncHandler = require('express-async-handler');
-const axios = require('axios');
+const { timer } = require('cas-common-lib');
 const { MailChimp } = require('../classes/Main');
 const resHandler = require('../services/resHandler');
 
 // GET /
 router.get('/', asyncHandler(async (req, res, next) => {
-  let result = await axios.get(`${process.env.MC_API_URL}campaigns?&count=100&since_send_time=2020-01-01 00:00:00&list_id=ff7c1a4442&sort_field=send_time`, {
-    auth: {
-      username: process.env.MC_USERNAME,
-      password: process.env.MC_API_KEY
-    }
-  }).catch(err => console.log(err));
+  // Set request timeout as 10 mins
+  // The default timeout for nodejs is 2mins and it is not long enough for this request to be done
+  // Browser will automatically make another request after timeout and it will cause the script run again asynchronously
+  //
+  req.setTimeout(600000);
 
-  // console.log(result.data);
+  const startTime = new Date();
+
   let mailchimp = new MailChimp();
-  let data = await mailchimp.getListData();
-  console.log(data);
+  let data = await mailchimp.getCampaignData("cas", 300, "2020-01-01 00:00:00", "send_time");
+  let campaigns = data.campaigns;
 
-  await resHandler.handleRes(req, res, next, 200, "done");
+  let campaignData = [], invalidCampaign = [];
+  for (let i = 0; i < campaigns.length; ++i) {
+    if (campaigns[i].type === "regular") {
+      campaignData.push({
+        _id: campaigns[i].id,
+        list_id: process.env.CAS_AUDIENCE_LIST_ID,
+        title: campaigns[i].settings.title,
+        type: campaigns[i].type,
+        subject_line: campaigns[i].settings.subject_line,
+        preview_text: campaigns[i].settings.preview_text,
+        send_time: campaigns[i].send_time,
+        google_analytics: `${campaigns[i].id}-${campaigns[i].tracking.google_analytics}`,
+        content: (await mailchimp.getCampaignContentData(campaigns[i].id)).data.plain_text
+      });
+      await timer.timeout(1000);
+    }
+    else if (campaigns[i].type === "variate") {
+      campaignData.push({
+        _id: campaigns[i].id,
+        list_id: process.env.CAS_AUDIENCE_LIST_ID,
+        title: campaigns[i].settings.title,
+        type: campaigns[i].type,
+        google_analytics: campaigns[i].tracking.google_analytics,
+        variate_settings: campaigns[i].variate_settings,
+        variate_contents: (await mailchimp.getCampaignContentData(campaigns[i].id)).data.variate_contents
+      });
+      await timer.timeout(1000);
+    }
+    else {
+      invalidCampaign.push({
+        id: campaigns[i].id,
+        type: campaigns[i].type
+      });
+    }
+  }
+
+  const endTime = new Date();
+  let usedTime = endTime - startTime;
+
+  console.log(campaignData.length);
+  console.log(`${usedTime} ms is used`);
+
+  if (invalidCampaign.length > 0) {
+    console.log("Here is the list of campaign with invalid campaign type");
+    console.log(invalidCampaign);
+  }
+
+  res.json(await mailchimp.insertCampaignData(campaignData));
+
+  // await resHandler.handleRes(req, res, next, 200, "done");
 }));
 
 module.exports = router;
