61d6c8a7d47909d0fce48e296725a6a956e42707
Andrew Y <andrew@compandsave.com>
2020-12-10 15:40:25 -0800

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 npm install -g serverless
 npm install --save-dev serverless-domain-manager
-serverless deploy --stage $env --package \   $CODEBUILD_SRC_DIR/target/$env -v
+serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v
