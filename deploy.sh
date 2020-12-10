83488d5b3a1cf92160d45391d180f8ebf3ae5fb9
Andrew Y <andrew@compandsave.com>
2020-12-10 15:50:25 -0800

    add ls

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+ls -al
+ls -al $CODEBUILD_SRC_DIR/target/$env
 npm install -g serverless
 npm install --save-dev serverless-domain-manager
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v
