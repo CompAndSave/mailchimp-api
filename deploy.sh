8712eb000f1b79cd89c32c5dc5dc97466c49d8a4
Andrew Y <andrew@compandsave.com>
2020-12-10 16:38:26 -0800

    remove custom manager

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@
 ls -al
 ls -al $CODEBUILD_SRC_DIR/target/$env
 npm install -g serverless
-npm install --save-dev serverless-domain-manager
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v
